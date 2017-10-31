f9fdde819a23e661ade2493f4c9957c8a52c3255
Pani Aravind K V <Aravind@Panis-MacBook-Pro.local>
2017-10-30 17:20:59 -0700

    response from solr

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,13 @@
 let data = require("./data.json");
+let SolrNode = require('solr-node');
+
+let client = new SolrNode({
+    host: 'ec2-34-213-252-195.us-west-2.compute.amazonaws.com',
+    port: '8983',
+    core: 'mycore',
+    protocol: 'http',
+    debugLevel: 'ERROR'
+});
 
 module.exports = function(app) {
 
@@ -24,7 +33,37 @@ module.exports = function(app) {
 
     app.get('/search', function(req, res) {
         console.log(req.query.q);
-        res.contentType('json');
-        res.send({ some: JSON.stringify({ response: 'json' }) });
+        const textLimit = 70;
+        let queryText = req.query.q.replace(/[^\w\s|\.]/g, " ").replace(/\s+/g, " ");
+
+        strQuery = client.query().q('content:' + queryText);
+        client.search(strQuery, function(err, result) {
+            if (err) {
+                console.log(err);
+                res.send(500);
+            }
+
+            let docs = result.response.docs;
+            let question = {};
+
+            if (req.query.qId != undefined && req.query.qId != "") {
+                let i = parseInt(req.query.qId);
+                if (data[i] != undefined && data[i] != null) {
+                    question = data[i];
+                    if (question.text.length > textLimit - 3) {
+                        question.textTrimmed = question.text.substring(0, textLimit - 3) + "...";
+                    } else {
+                        question.textTrimmed = question.text;
+                    }
+                }
+            }
+
+            res.contentType('json');
+            res.send({
+                docs: JSON.stringify(docs),
+                question: JSON.stringify(question),
+                searchText: queryText
+            });
+        });
     });
 }
